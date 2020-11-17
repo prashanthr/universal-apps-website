9e45e658ece6422ac16902cbb6459be45ee1246c
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-11-17 14:31:55 -0800

    feat(bootstrap): Bootstrap app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <p className="slogan">
+        We build apps for the world.
+      </p>
+    </div>
+  );
+}
+
+export default App;
