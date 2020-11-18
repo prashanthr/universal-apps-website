f71864cec85a24043a50cf38573042c13a57ef0f
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-11-17 16:53:19 -0800

    feat(boostrap): Bootstrap site (#1)
    Co-authored-by: Prashanth R <1409501+prashanthr@users.noreply.github.com>

---
+++ src/client/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './app.css'
+
+function App() {
+  return (
+    <div>
+      <p className="slogan">
+        We build apps for the world.
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/server/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import http from 'http'
+import path from 'path'
+import cors from 'cors'
+import { setupRoutes }  from './routes'
+
+const app = express()
+const port = 9000
+
+app.use(cors())
+app.use(express.static(path.join(__dirname, '/../../build')))
+setupRoutes(app)
+
+const server = async () => {
+  let httpServer = http.Server(app)
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname, '/../../build', 'index.html'))
+  })
+  httpServer.listen(port, () => {
+    console.log(`universal-apps server listening on port ${port}!`)
+  })
+}
+server()
