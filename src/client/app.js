2cc9f7911b345423b09ddddae235aab02bf2ef61
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-12-30 12:22:38 -0800

    fix css

--- src/client/app.js
+++ src/client/app.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import LogoImg from '../client/assets/vector/default-monochrome.svg'
 import './app.css'
+
+const Logo = () => (
+  <div className='universal-apps-logo'>
+    <a href={'/'}>
+      <img src={LogoImg} alt='logo' />
+    </a>
+  </div>
+)
+
+const Footer = () => (
+  <div className='universal-apps-footer'>
+    Copyright Universal Apps &copy; {(new Date().getFullYear())}
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <p className="slogan">
+      <Logo />
+      <div className='slogan'>
         We build apps for the world.
-      </p>
+      </div>
+      <Footer />
     </div>
   );
 }
